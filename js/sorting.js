56800d95c5101272d47a5f32abafcab7120cf28a
dheeraj-deb <dheerajt9539@gmail.com>
2023-05-01 22:44:41 +0530

    bubble sort implimented

--- js/sorting.js
+++ js/sorting.js
@@ -2,45 +2,37 @@ const barContainer = document.getElementById('bar_container');
 const newArrayBtn = document.getElementById('new_array');
 const bubbleSort = document.getElementById('bubble');
 
-let bars = new Array()
-let divs = new Array()
+let bars = new Array();
 
 function swap(el1, el2) {
-    // el1.style.background = "black"
-    // el2.style.background = "black"
-
     return new Promise(resolve => setTimeout(() => {
         const style1 = window.getComputedStyle(el1);
         const style2 = window.getComputedStyle(el2);
 
-        const transform1 = style1.getPropertyValue("height");
-        const transform2 = style2.getPropertyValue("height");
+        const height1 = style1.getPropertyValue("height");
+        const height2 = style2.getPropertyValue("height");
 
-        console.log(transform1)
+        el1.style.height = height2;
+        el2.style.height = height1;
 
-        el1.style.height = transform2;
-        el2.style.height = transform1;
-
-        el1.style.background = "green"
-        el2.style.background = "green"
+        el1.style.background = "red"
+        el2.style.background = "red"
         resolve()
-    }, 1000))
+    }, 20))
 }
 
 function start() {
     for (let i = 0; i <= 100; i++) {
-        bars.push(Math.floor(Math.random() * 500))
+        bars.push(Math.floor(Math.random() * 200))
     }
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < bars.length; i++) {
         const div = document.createElement('div');
         div.className = 'bar';
         div.style.height = `${bars[i]}px`;
         barContainer.appendChild(div);
-        divs.push(div);
     }
 }
-start()
 
 newArrayBtn.addEventListener("click", () => {
     barContainer.innerHTML = ''
@@ -48,5 +40,23 @@ newArrayBtn.addEventListener("click", () => {
     start()
 })
 
+// bubble sort
 bubbleSort.addEventListener('click', async () => {
+    let nodes = barContainer.childNodes;
+    let i, j;
+    for (i = 0; i < nodes.length - 1; i++) {
+        for (j = 1; j < nodes.length - i - 1; j++) {
+            if (parseInt(nodes[j].style.height) > parseInt(nodes[j + 1].style.height)) {
+                nodes[j].style.background = "black";
+                nodes[j + 1].style.background = "black";
+                await swap(nodes[j], nodes[j + 1])
+                // nodes[j + 1].style.background = "green";
+            } else {
+                nodes[j].style.background = "red";
+                nodes[j + 1].style.background = "red";
+            }
+        }
+    }
 })
+
+start()
